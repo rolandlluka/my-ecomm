54a93b253278b25d1649e938f194332f8e8d80a1
Roland Lluka <rolandlluka@gmail.com>
2024-07-08 19:17:24 +0200

    Create home page with categories

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class Category < ApplicationController
   def index
     @main_categories = Category.take(4)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   end
 
   get "admin" => "admin#index"
+  get "category" => "category#index"
 end
